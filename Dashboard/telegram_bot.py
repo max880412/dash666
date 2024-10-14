58d0267b47137fb25bf246c40f56ba5dc62f5679
max880412 <75732414+max880412@users.noreply.github.com>
2024-10-14 15:34:27 -0400

    Update telegram_bot.py

--- Dashboard/telegram_bot.py
+++ Dashboard/telegram_bot.py
@@ -19,7 +19,7 @@ def start(update: Update, context: CallbackContext):
 # Configuración del bot
 def main():
     # Usa el token de tu bot de Telegram
-    TOKEN = 'TU_BOT_TOKEN'
+    TOKEN = '7237293197:AAFgcNTu9ehfih8iSIX4ZezE-WOux65RAbs'
     
     updater = Updater(TOKEN, use_context=True)
 
